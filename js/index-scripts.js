758e57c1ea0e4dc79ae6cf55fbf52e35b6bf2318
Thomas Hennessy <p16176035@my365.dmu.ac.uk>
2020-09-28 16:24:41 +0100

    changed index page slideshow

--- js/index-scripts.js
+++ js/index-scripts.js
@@ -1,21 +1,17 @@
 "use strict";
 
-// Slide show scripts
-// https://www.w3schools.com/w3css/w3css_slideshow.asp
-// https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_rr
 
-var slideIndex = 0;
+var slideIndex = 0; //Slide selector
+var imageArray = document.getElementsByClassName('slideshow-slide'); //Gathering all images labeled 'slideshow-slide'
+
 slideshow();
 
-//Operates the automated slideshow on the index page
-function slideshow() {
-  var i;
-  var x = document.getElementsByClassName('slideshow-slide'); //gathers the eliments denoted by the class slideshow-slide
-  for (i = 0; i < x.length; i++){
-    x[i].style.display = "none"; //display none of the slides
-  }
+function slideshow(){
+  imageArray[slideIndex].style.display = "none"; //Set curent image to none
   slideIndex++; //go to next slide
-  if (slideIndex > x.length) {slideIndex = 1} //when reaching the end of the slides, go back to the first
-  x[slideIndex-1].style.display = "block"; //display the selected slide
-  setTimeout(slideshow, 5000); //run slideshow again after 5 seconds
+  slideIndex %= imageArray.length; //do not go past the last slide (last image +1). If you do, go back to the first
+
+  imageArray[slideIndex].style.display = "block"; //set the selected slide to block
+
+  setTimeout(slideshow, 5000); //repeat process every 5 seconds
 }
