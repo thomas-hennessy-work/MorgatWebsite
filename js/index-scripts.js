56d7f57ae234092e92ae3172bf8360275a853a18
Thomas Hennessy <p16176035@my365.dmu.ac.uk>
2020-09-28 11:34:27 +0100

    index auto slideshow

---
+++ js/index-scripts.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Slide show scripts
+//https://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_slideshow_rr
+
+var slideIndex = 0;
+slideshow();
+
+function slideshow() {
+  var i;
+  var x = document.getElementsByClassName('slideshow-slide'); //gathers the eliments denoted by the class slideshow-slide
+  for (i = 0; i < x.length; i++){
+    x[i].style.display = "none"; //display none of the slides
+  }
+  slideIndex++; //go to next slide
+  if (slideIndex > x.length) {slideIndex = 1} //when reaching the end of the slides, go back to the first
+  x[slideIndex-1].style.display = "block"; //display the selected slide
+  setTimeout(slideshow, 5000); //run slideshow again after 5 seconds
+}
