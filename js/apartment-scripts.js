0d4afe4bc6062c9239ee17e854fe92d9d8440dc8
Thomas Hennessy <p16176035@my365.dmu.ac.uk>
2020-10-02 13:48:22 +0100

    next and previous script complete

--- js/apartment-scripts.js
+++ js/apartment-scripts.js
@@ -22,22 +22,53 @@ function nextButtonListeners(nextButton, idDisplay, slideshowList){
   var newButton = document.getElementById(nextButton);
   var image = document.getElementById(idDisplay);
 
-  newButton.addEventListener("click", function nextListeners(idDisplay, slideshowList){
-    var nextImageIDX = slideshowList.src.indexOf(image.src) + 1;
-
-    if(nextImageIDX == slideshowList.length + 1){
-      image.src = slideshowList[0].src;
-    } else {
-      image.src = slideshowList[nextImageIDX].src;
+  newButton.addEventListener("click", function nextListener(){
+    for(var i = 0; slideshowList.length > i; i++){ //looping through all selector images
+      if(slideshowList[i].src == image.src){ //atempting to match the selecotr image to the display image
+        if((i + 2) > slideshowList.length){//if it is the last selector image in the list, restart
+          var nextIDX = 0;
+        } else { //go to next selector image
+          var nextIDX = i+1;
+        }
+      }
     }
+    image.src = slideshowList[nextIDX].src; //asign selector image to the display image
 
+    for(var x = 0; slideshowList.length > x ; x++){ //selector images styling
+      slideshowList[x].style.opacity = "0.4";
+    }
+    slideshowList[nextIDX].style.opacity = "1";
+  });
+}
+
+function prevButtonListeners(nextButton, idDisplay, slideshowList){
+  var newButton = document.getElementById(nextButton);
+  var image = document.getElementById(idDisplay);
+
+  newButton.addEventListener("click", function nextListener(){
+    for(var i = 0; slideshowList.length > i; i++){ //looping through all selector images
+      if(slideshowList[i].src == image.src){ //atempting to match the selecotr image to the display image
+        if((i - 1) < 0){//if it is the last selector image in the list, restart
+          var nextIDX = slideshowList.length - 1;
+        } else { //go to next selector image
+          var nextIDX = i-1;
+        }
+      }
+    }
+    image.src = slideshowList[nextIDX].src; //asign selector image to the display image
+
+    for(var x = 0; slideshowList.length > x ; x++){ //selector images styling
+      slideshowList[x].style.opacity = "0.4";
+    }
+    slideshowList[nextIDX].style.opacity = "1";
   });
 }
 
 
 var slideshowA = document.getElementsByClassName('slideshowA-img'); //gathers all the selector images in to an array
 slideshowListners("apartment-A-display", "apartment-A-link", slideshowA); //Passes the class of the display image, the class of the image link and list of selector images
-nextButtonListeners("A-next", "apartment-A-link", slideshowA);
+nextButtonListeners("A-next", "apartment-A-display", slideshowA);
+prevButtonListeners("A-previous", "apartment-A-display", slideshowA);
 
 var slideshowB = document.getElementsByClassName('slideshowB-img');
 slideshowListners("apartment-B-display", "apartment-B-link", slideshowB);
