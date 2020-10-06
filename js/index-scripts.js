f3ff188cb702b82f967bf30359beec4e3e7bbed9
Thomas Hennessy <p16176035@my365.dmu.ac.uk>
2020-10-06 10:22:17 +0100

    added images, modified auto slide function to re-useable

--- js/index-scripts.js
+++ js/index-scripts.js
@@ -1,17 +1,17 @@
 "use strict";
 
-
-var slideIndex = 0; //Slide selector
-var imageArray = document.getElementsByClassName('slideshow-slide'); //Gathering all images labeled 'slideshow-slide'
-
-slideshow();
-
-function slideshow(){
+function slideshow(imageArray, slideIndex){
+  console.log(slideIndex);
+  console.log(imageArray[slideIndex]);
   imageArray[slideIndex].style.display = "none"; //Set curent image to none
   slideIndex++; //go to next slide
   slideIndex %= imageArray.length; //do not go past the last slide (last image +1). If you do, go back to the first
 
   imageArray[slideIndex].style.display = "block"; //set the selected slide to block
 
-  setTimeout(slideshow, 5000); //repeat process every 5 seconds
+  setTimeout(function(){slideshow(imageArray, slideIndex)}, 5000); //repeat process every 5 seconds
 }
+
+var slidePos = 0;
+var imageItems = document.getElementsByClassName('slideshow-slide'); //Gathering all images labeled 'slideshow-slide'
+slideshow(imageItems, slidePos);
